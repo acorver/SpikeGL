a3dae0c8f916e8841b6fd82e331ade8249bb3b7f
Calin A. Culianu <calin.culianu@gmail.com>
2009-05-11 12:10:21 -0400

    Bumped version made par2.exe +x

--- Version.h
+++ Version.h
@@ -1,7 +1,7 @@
 #ifndef Version_H
 #define Version_H
 
-#define VERSION 0x20090509
-#define VERSION_STR "LeoDAQGL v.20090509"
+#define VERSION 0x20090511
+#define VERSION_STR "LeoDAQGL v.20090511"
 
 #endif
