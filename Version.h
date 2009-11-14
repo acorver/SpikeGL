db6db94dcdcc689d2a60ed7abc5641ae9e6ed886
Calin Culianu <Calin.Culianu@gmail.com>
2009-11-14 12:04:53 -0500

    bumped version

--- Version.h
+++ Version.h
@@ -1,7 +1,7 @@
 #ifndef Version_H
 #define Version_H
 
-#define VERSION 0x20091109
-#define VERSION_STR "LeoDAQGL v.20091109"
+#define VERSION 0x20091114
+#define VERSION_STR "LeoDAQGL v.20091114"
 
 #endif
