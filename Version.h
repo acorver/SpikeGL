267e273a6a1bde9d8e4ac288198e0c2d8a5db16e
Calin Culianu <Calin.Culianu@gmail.com>
2012-10-19 20:23:44 +0300

    tweak

--- Version.h
+++ Version.h
@@ -1,7 +1,7 @@
 #ifndef Version_H
 #define Version_H
 
-#define VERSION 0x20120919
-#define VERSION_STR "SpikeGL v.20120919"
+#define VERSION 0x20121019
+#define VERSION_STR "SpikeGL v.20121019"
 
 #endif
