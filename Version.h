3e10f8e82192365af08228cdb2ae3f4a52989f81
Calin Culianu <Calin.Culianu@gmail.com>
2015-02-08 11:36:32 +0200

    version number update

--- Version.h
+++ Version.h
@@ -1,7 +1,7 @@
 #ifndef Version_H
 #define Version_H
 
-#define VERSION 0x20150205
-#define VERSION_STR "SpikeGL v.20150205"
+#define VERSION 0x20150208
+#define VERSION_STR "SpikeGL v.20150208"
 
 #endif
