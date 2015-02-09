7d304a3cd0faa4d625e2f892b0f4cc7645ecc750
Calin Culianu <calin.culianu@gmail.com>
2015-02-09 17:28:22 +0200

    version bump

--- Version.h
+++ Version.h
@@ -1,7 +1,7 @@
 #ifndef Version_H
 #define Version_H
 
-#define VERSION 0x20150208
-#define VERSION_STR "SpikeGL v.20150208"
+#define VERSION 0x20150209
+#define VERSION_STR "SpikeGL v.20150209"
 
 #endif
