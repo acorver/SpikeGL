fb955b72231df8a5b71f33b9f3f6949ccf807d4a
Calin Culianu <calin.culianu@gmail.com>
2015-03-30 15:46:17 +0200

    tweaks

--- DAQ.cpp
+++ DAQ.cpp
@@ -2021,7 +2021,7 @@ namespace DAQ
 	{
 		QStringList files;
         //files.push_back(QString(":/FG/FrameGrabber/x64/Release/") + exeName);
-        files.push_back(QString(":/FG/FrameGrabber/FG_SpikeGL/x64/Debug/") + exeName);
+        files.push_back(QString(":/FG/FrameGrabber/FG_SpikeGL/x64/Release/") + exeName);
 		files.push_back(":/FG/FrameGrabber/x64/Release/SapClassGui75.NET_2013.dll");
 		files.push_back(":/FG/FrameGrabber/J_2000+_Electrode_8tap_8bit.ccf");
 		return files;
