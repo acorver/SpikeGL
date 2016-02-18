4f5f6c4a958f86176da005d1eaca5c5af910a21d
Calin Culianu <Calin.Culianu@gmail.com>
2016-02-18 11:19:13 +0200

    tweaks

--- Version.h
+++ Version.h
@@ -1,6 +1,12 @@
 #ifndef Version_H
 #define Version_H
 
-#define VERSION 0x20161702
-#define VERSION_STR "SpikeGL v.20161702"
+#define VERSION 0x20161802
+#ifdef WIN64
+#  define VERSION_STR "SpikeGL Win64 v.20161802"
+#else
+#  define VERSION_STR "SpikeGL v.20161802"
+#endif
+
+
 #endif
