47ea320d583b26054fbcf6128e888f26b1c20c13
Calin Culianu <calin.culianu@gmail.com>
2016-06-14 10:19:59 -0400

    bumped version

--- Version.h
+++ Version.h
@@ -1,13 +1,13 @@
 #ifndef Version_H
 #define Version_H
 
-#define VERSION 0x20161305
+#define VERSION 0x20161406
 #ifdef WIN64
-#  define VERSION_STR "SpikeGL Win64 v.20161305"
+#  define VERSION_STR "SpikeGL Win64 v.20161406"
 #elif defined(MACX)
-#  define VERSION_STR "SpikeGL OSX v.20161305"
+#  define VERSION_STR "SpikeGL OSX v.20161406"
 #else
-#  define VERSION_STR "SpikeGL v.20161305"
+#  define VERSION_STR "SpikeGL v.20161406"
 #endif
 
 
