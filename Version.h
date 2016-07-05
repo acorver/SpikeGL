1e00c31d05f5c676e829da793777fc998ce0a09a
Calin Culianu <calin.culianu@gmail.com>
2016-07-05 07:40:22 -0700

    oops.. wrong date format for version.. fixed

--- Version.h
+++ Version.h
@@ -1,13 +1,13 @@
 #ifndef Version_H
 #define Version_H
 
-#define VERSION 0x20160507
+#define VERSION 0x20160705
 #ifdef WIN64
-#  define VERSION_STR "SpikeGL Win64 v.20160507"
+#  define VERSION_STR "SpikeGL Win64 v.20160705"
 #elif defined(MACX)
-#  define VERSION_STR "SpikeGL OSX v.20160507"
+#  define VERSION_STR "SpikeGL OSX v.20160705"
 #else
-#  define VERSION_STR "SpikeGL v.20160507"
+#  define VERSION_STR "SpikeGL v.20160705"
 #endif
 
 
